9052d5924658514f7abd3599180373faf218612c
euro <57731147+evro@users.noreply.github.com>
2020-03-05 11:17:59 -0500

    Speed.py
    If you want it to speed again or adjust time.sleep's time. 
    Repeating the same speed from the speed I inputted, just run the script again.

---
+++ Speed.py
@@ -0,0 +1,19 @@
+import pyautogui import time
+
+# Presses the space key
+pyautogui.press("space")
+
+# Presses the w key once
+pyautogui.press("w")
+
+# Presses the enter key once
+pyautogui.press("enter")
+
+# Ends it from going too fast, if you want to risk dying in game, change time.sleep's time.
+time.sleep (0.5)
+
+# Presses enter one last time.
+pyautogui.press("enter")
+
+end
+end
